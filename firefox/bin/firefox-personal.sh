bd11ed7b3a9b5678689ffd705d84ba77308daf33
Mike Chevett <mchevett@gmail.com>
2025-11-10 09:53:53 -0500

    updates for firefox profiles

---
+++ firefox/bin/firefox-personal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/firefox --class "firefox-personal" -new-instance -P PERSONAL "$@"
---
+++ firefox/bin/firefox-work.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/firefox --class "firefox-work" -new-instance -P WORK "$@"
