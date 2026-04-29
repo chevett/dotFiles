17075b5bf48692dfb5339082b2954ae2aeef0668
Mike Chevett <mchevett@gmail.com>
2026-04-29 10:08:43 -0400

    F10/F12 wezterm bindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -21,9 +21,38 @@ config.window_decorations = 'RESIZE'
 config.window_padding = { left = 8, right = 8, top = 4, bottom = 4 }
 config.scrollback_lines = 10000
 
+-- Ctrl+Click to open URLs
+config.mouse_bindings = {
+  {
+    event = { Down = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = wezterm.action.Nop,
+  },
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = wezterm.action_callback(function(window, _pane)
+      window:toast_notification('WezTerm', 'Ctrl+Click fired', nil, 3000)
+    end),
+  },
+}
+
 -- Keybindings
 config.keys = {
+  { key = 'F10', action = wezterm.action.ActivateCommandPalette },
   { key = 'F11', action = wezterm.action.ToggleFullScreen },
+  {
+    key = 'F12',
+    action = wezterm.action_callback(function(window, _pane)
+      if wezterm.GLOBAL.maximized then
+        window:restore()
+        wezterm.GLOBAL.maximized = false
+      else
+        window:maximize()
+        wezterm.GLOBAL.maximized = true
+      end
+    end),
+  },
 }
 
 return config
